446b1d109a18f59a7bb3def2a066d28dc92e01bc
Yusuf Shaikh <yusuf.shaikh0719@gmail.com>
2024-10-13 13:25:56 -0700

    config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,12 +4,12 @@ import { getDatabase } from 'firebase/database';
 const firebaseConfig = {
   apiKey: "AIzaSyB6fs6lB-87nCH36O6-Zfvnwf-miKyXz8E",
   authDomain: "fusionbite.firebaseapp.com",
-  databaseURL: "https://fusionbite-default-rtdb.firebaseio.com/",
+  databaseURL: "https://fusionbite-default-rtdb.firebaseio.com",
   projectId: "fusionbite",
   storageBucket: "fusionbite.appspot.com",
   messagingSenderId: "251727286478",
-  appId: "1:251727286478:web:e99fe5b1b6f3815740009f",
-  measurementId: "G-DKB2E420WE"
+  appId: "1:251727286478:web:05b5e5f513fe001e40009f",
+  measurementId: "G-SL7G1FXW4Q"
 };
 
 const app = initializeApp(firebaseConfig);
